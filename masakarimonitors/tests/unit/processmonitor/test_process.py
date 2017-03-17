7aef952a4153c810ac290813d6830b7faec7221c
Kengo Takahara <takahara.kengo.z03@as.ntts.co.jp>
2017-03-17 17:52:23 +0900

    Add testcases of ha and processmonitor
    This patch added missing testcases for ha and processmonitor
    directory. And also improved existing testcases for them.
    
    Change-Id: Ifd9681b53b71122aa9ca738e6e6bf49f282b1f61

--- masakarimonitors/tests/unit/processmonitor/test_process.py
+++ masakarimonitors/tests/unit/processmonitor/test_process.py
@@ -47,6 +47,19 @@ MOCK_PROCESS_LIST = [
     },
 ]
 
+MOCK_DOWN_PROCESS_LIST = [
+    {
+        'process_name': 'mock_process_name_A',
+        'start_command': 'mock_start_command',
+        'pre_start_command': 'mock_pre_start_command',
+        'post_start_command': 'mock_post_start_command',
+        'restart_command': 'mock_restart_command',
+        'pre_restart_command': 'mock_pre_restart_command',
+        'post_restart_command': 'mock_post_restart_command',
+        'run_as_root': True
+    },
+]
+
 
 class TestProcessmonitorManager(testtools.TestCase):
 
@@ -59,21 +72,102 @@ class TestProcessmonitorManager(testtools.TestCase):
         else:
             return
 
+    @mock.patch.object(eventlet.greenthread, 'sleep')
+    @mock.patch.object(handle_process.HandleProcess, 'restart_processes')
     @mock.patch.object(handle_process.HandleProcess, 'monitor_processes')
     @mock.patch.object(handle_process.HandleProcess, 'start_processes')
     @mock.patch.object(handle_process.HandleProcess, 'set_process_list')
     @mock.patch.object(yaml, 'load')
+    @mock.patch('__builtin__.file')
     def test_main(self,
+                  mock_file,
                   mock_load,
                   mock_set_process_list,
                   mock_start_processes,
-                  mock_monitor_processes):
+                  mock_monitor_processes,
+                  mock_restart_processes,
+                  mock_sleep):
 
+        mock_file.return_value = None
         mock_load.side_effect = [self._get_mock_process_list(0),
+                                 self._get_mock_process_list(0),
                                  self._get_mock_process_list(1)]
         mock_set_process_list.return_value = None
         mock_start_processes.return_value = None
-        mock_monitor_processes.return_value = []
+        mock_monitor_processes.side_effect = [MOCK_DOWN_PROCESS_LIST, []]
+        mock_restart_processes.return_value = None
+        mock_sleep.return_value = None
 
         obj = processmonitor_manager.ProcessmonitorManager()
         obj.main()
+
+        mock_set_process_list.assert_called_with(MOCK_PROCESS_LIST)
+        mock_start_processes.assert_called_once_with()
+        self.assertEqual(2, mock_monitor_processes.call_count)
+        mock_restart_processes.assert_called_once_with(MOCK_DOWN_PROCESS_LIST)
+
+    @mock.patch.object(handle_process.HandleProcess, 'restart_processes')
+    @mock.patch.object(handle_process.HandleProcess, 'monitor_processes')
+    @mock.patch.object(handle_process.HandleProcess, 'start_processes')
+    @mock.patch.object(handle_process.HandleProcess, 'set_process_list')
+    @mock.patch.object(yaml, 'load')
+    @mock.patch('__builtin__.file')
+    def test_main_exception(self,
+                            mock_file,
+                            mock_load,
+                            mock_set_process_list,
+                            mock_start_processes,
+                            mock_monitor_processes,
+                            mock_restart_processes):
+
+        mock_file.return_value = None
+        mock_load.return_value = self._get_mock_process_list(0)
+        mock_set_process_list.return_value = None
+        mock_start_processes.side_effect = Exception("Test exception.")
+
+        obj = processmonitor_manager.ProcessmonitorManager()
+        obj.main()
+
+        mock_set_process_list.assert_called_once_with(MOCK_PROCESS_LIST)
+        mock_start_processes.assert_called_once_with()
+        mock_monitor_processes.assert_not_called()
+        mock_restart_processes.assert_not_called()
+
+    @mock.patch.object(handle_process.HandleProcess, 'set_process_list')
+    @mock.patch.object(yaml, 'load')
+    @mock.patch('__builtin__.file')
+    def test_load_process_list_yaml_error(self,
+                                         mock_file,
+                                         mock_load,
+                                         mock_set_process_list):
+
+        mock_file.return_value = None
+        mock_load.side_effect = yaml.YAMLError
+
+        obj = processmonitor_manager.ProcessmonitorManager()
+        obj.main()
+
+        mock_set_process_list.assert_not_called()
+
+    @mock.patch.object(handle_process.HandleProcess, 'set_process_list')
+    @mock.patch.object(yaml, 'load')
+    @mock.patch('__builtin__.file')
+    def test_load_process_list_exception(self,
+                                         mock_file,
+                                         mock_load,
+                                         mock_set_process_list):
+
+        mock_file.return_value = None
+        mock_load.side_effect = Exception("Test exception.")
+
+        obj = processmonitor_manager.ProcessmonitorManager()
+        obj.main()
+
+        mock_set_process_list.assert_not_called()
+
+    def test_stop(self):
+
+        obj = processmonitor_manager.ProcessmonitorManager()
+        obj.stop()
+
+        self.assertFalse(obj.running)
