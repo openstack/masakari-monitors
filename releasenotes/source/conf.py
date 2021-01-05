9768c7dacd7e18fe93dad3be01c81eeddef41f9b
XinxinShen <shenxinxin@inspur.com>
2021-01-05 14:28:09 +0800

    remove unicode from code
    Change-Id: I41eec434ef141764fa8f8f0dcafd3dfe3f1ee6b9

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -55,8 +55,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'masakarimonitors Release Notes'
-copyright = u'2016, OpenStack Foundation'
+project = 'masakarimonitors Release Notes'
+copyright = '2016, OpenStack Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -212,8 +212,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    ('index', 'MasakarimonitorsReleaseNotes.tex', u'Masakarimonitors Release'
-     u'Notes Documentation', u'Masakarimonitors Developers', 'manual'),
+    ('index', 'MasakarimonitorsReleaseNotes.tex', 'Masakarimonitors Release'
+     'Notes Documentation', 'Masakarimonitors Developers', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -242,8 +242,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'Masakarimonitorsreleasenotes', u'Masakarimonitors Release Notes'
-     u'Documentation', [u'Masakarimonitors Developers'], 1)
+    ('index', 'Masakarimonitorsreleasenotes', 'Masakarimonitors Release Notes'
+     'Documentation', ['Masakarimonitors Developers'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -256,8 +256,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', 'MasakarimonitorsReleaseNotes', u'Masakarimonitors Release Notes'
-     u'Documentation', u'Masakarimonitors Developers',
+    ('index', 'MasakarimonitorsReleaseNotes', 'Masakarimonitors Release Notes'
+     'Documentation', 'Masakarimonitors Developers',
      'MasakarimonitorsReleaseNotes', 'One line description of project.',
      'Miscellaneous'),
 ]
